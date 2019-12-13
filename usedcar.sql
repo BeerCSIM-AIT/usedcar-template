bd4cfd89efc637cb5d433e6359b722bfa448d7b3
BeerCSIM-AIT <st121032@ait.ac.th>
2019-12-13 19:40:33 +0700

    update template and db

--- usedcar.sql
+++ usedcar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 11, 2019 at 07:23 AM
+-- Generation Time: Dec 13, 2019 at 04:35 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.1.30
 
@@ -30,12 +30,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `car` (
   `id` int(11) NOT NULL,
+  `carType` int(11) NOT NULL,
   `brand` varchar(50) NOT NULL,
   `model` varchar(80) NOT NULL,
   `color` varchar(20) NOT NULL,
   `license` varchar(20) NOT NULL,
   `province` varchar(30) NOT NULL,
   `modelYear` varchar(4) NOT NULL,
+  `price` int(11) NOT NULL,
   `postedBy` int(11) NOT NULL,
   `postedDate` datetime NOT NULL,
   `carpic` varchar(100) NOT NULL
@@ -56,16 +58,6 @@ CREATE TABLE `customers` (
   `email` varchar(1024) NOT NULL,
   `active` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`id`, `username`, `password`, `firstname`, `lastname`, `email`, `active`) VALUES
-(1, 'Alex', '5f4dcc3b5aa765d61d8327deb882cf99', 'Alex', 'Garnett', 'alex.g@gmail.com', 0),
-(2, 'BeerBC16', '9032d29da6f07136c5d5c51699157bc9', 'Phakpoom', 'Ittirattanakomon', 'beerbc16@gmail.com', 0),
-(4, 'Supanan.T', 'b2dd0eac7e66799028f2c247fac22ed3', 'Supanan', 'Tongrak', 'supanan.t@gmail.com', 0),
-(5, 'AekThanatorn', '25d55ad283aa400af464c76d713c07ad', 'Thanatorn', 'Jeungrunruengkit', 'thana@gmail.com', 0);
 
 --
 -- Indexes for dumped tables
@@ -97,7 +89,7 @@ ALTER TABLE `car`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
